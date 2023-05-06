53e899df06c2fe9c80e47bc16eaf8bfeff1ca252
Logan H-D <notquiteapex@gmail.com>
2023-05-06 03:47:06 -0400

    update some old crates, start on CLI

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,70 @@
 extern crate clap;
 extern crate dirs;
 
+use std::path::PathBuf;
+
+use clap::{Args, Parser, Subcommand, ValueEnum};
+
 mod config;
 mod util;
 
-fn deffered_main() -> Result<(), util::ExitMsg> {
-    // Load the environment variables from cargo for this info.
-    const AUTHORS: Option<&'static str> = option_env!("CARGO_PKG_AUTHORS");
-    const VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
+#[derive(Debug, Parser)]
+#[command(name = "VodBot", author, version)]
+#[command(about = "A video and chat manager for Twitch.tv", long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    config: Option<PathBuf>,
 
-    println!(
-        "{} {}",
-        AUTHORS.unwrap_or("UNKNOWN"),
-        VERSION.unwrap_or("UNKNOWN")
-    );
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    #[command(about="Initialize directories and files for VodBot")]
+    Init {
+
+    },
+    #[command(about="Get info about videos, clips, or channels")]
+    Info {
+
+    },
+    #[command(about="Pull videos, clips, chat logs, and more")]
+    Pull {
+        #[arg(value_enum, default_value_t=PullMode::All)]
+        mode: PullMode,
+    },
+    #[command(about="Stage video data for export or upload")]
+    Stage {
+        // command: StageCommands,
+    },
+    #[command(about="Export staged data to local storage")]
+    Export {
+        
+    },
+    #[command(about="Upload staged data to YouTube")]
+    Upload {
+        
+    },
+}
+
+#[derive(ValueEnum, Copy, Clone, Debug, PartialEq, Eq)]
+enum PullMode {
+    Vods,
+    Clips,
+    All,
+}
+
+
+fn deffered_main() -> Result<(), util::ExitMsg> {
+    // Testing the CLI args
+    let args = Cli::parse();
+    println!("{:?}", args);
 
     Ok(())
 }
 
 fn main() {
-    println!("Hello, world!");
-
     std::process::exit(deffered_main().map_or_else(
         |err| {
             print!("{}", err.msg.as_str());
