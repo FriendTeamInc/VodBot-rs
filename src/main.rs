b3485f31193c6ade41dde4494e74c131dc48d58b
Logan H-D <notquiteapex@gmail.com>
2023-05-05 23:21:54 -0400

    start using map for results

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
 // VodBot (c) 2020-23 Logan "NotQuiteApex" Hickok-Dickson
 
+use std::path::Path;
+
+extern crate clap;
+extern crate dirs;
+
+mod util;
+
+fn deffered_main() -> Result<(), util::ExitMsg> {
+	// Load the environment variables from cargo for this info.
+	const AUTHORS: Option<&'static str> = option_env!("CARGO_PKG_AUTHORS");
+	const VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
+
+    println!("{} {}", AUTHORS.unwrap_or("UNKNOWN"), VERSION.unwrap_or("UNKNOWN"));
+
+    Ok(())
+}
+
 fn main() {
     println!("Hello, world!");
+
+    std::process::exit(
+        deffered_main()
+        .map_or_else(|err| {
+            print!("{}", err.msg.as_str());
+            err.code as i32
+        }, |_| 0)
+    )
 }
---
+++ src/util.rs
@@ -0,0 +1,27 @@
+// Utility functions and types
+
+use std::fs;
+use std::path::Path;
+
+
+#[derive(Copy, Clone)]
+pub enum ExitCode {
+	_CleanExit,
+	CannotCreateDir,
+}
+
+pub struct ExitMsg {
+    pub msg: String,
+    pub code: ExitCode,
+}
+
+pub fn create_dir(dir_path: &Path) -> Result<(), ExitMsg> {
+    fs::create_dir_all(&dir_path)
+        .map_err(
+            |why| ExitMsg {
+                code: ExitCode::CannotCreateDir,
+                msg: format!("Cannot create directory `{}`, reason \"{}\".",
+                    &dir_path.display(), why)
+            }
+        )
+}
